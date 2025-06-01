3df76f38e4199f83e64b74433474c1e5d756923d
ftomista <fdtomista@up.edu.ph>
2025-06-01 23:18:17 +0800

    fixed type error in pytest

--- egg_rally/test_egg_entities.py
+++ egg_rally/test_egg_entities.py
@@ -1,17 +1,15 @@
-from egg_rally.egg_entities import Egg, EggEntity, Eggnemy, EggnemyList, EggnemyTag, EggnemyType, EggConfig
+from egg_rally.egg_entities import Egg, EggConfig
 from egg_rally.project_types import Hitbox, Vector, CartesianPoint
-
-
 class TestEgg:
     def test_egg_creation(self):
-        hitbox = Hitbox(CartesianPoint(0, 0), 3, 4)
-        egg_config = EggConfig(hitbox=hitbox,
-                               movement_speed=2.0,
-                               max_health=25,
-                               base_damage=1.0,
-                               damage_hitbox_scale=1.0,
-                               invincibility_frames=30)
-        egg = Egg(egg_config)
+        hitbox = Hitbox(CartesianPoint(0,0), 3, 4)
+        egg_config = EggConfig(hitbox = hitbox,
+                               movement_speed = 2.0,
+                               max_health = 25,
+                               base_damage = 1.0,
+                               damage_hitbox_scale = 1.0,
+                               invincibility_frames= 30)
+        egg = Egg(egg_config, 3)
         assert egg.hitbox == hitbox
         assert egg.movement_speed == 2.0
         assert egg.max_health == 25
@@ -19,48 +17,47 @@ class TestEgg:
         assert egg._damage_hitbox_scale == 1.0
         assert egg._invincibility_frames == 30
         assert egg._i_frame_counter == 0
-        assert egg._center_to_corner_vector == Vector(1.5, 2.0)
-
+        assert egg._center_to_corner_vector == Vector(1.5,2.0)
+        
         new_reference_point = CartesianPoint(0.0, 0.0)
         new_width_height_vector = Vector(3.0, 4.0)
-        assert egg._damage_hitbox == Hitbox(
-            new_reference_point, new_width_height_vector.x_hat, new_width_height_vector.y_hat)
+        assert egg._damage_hitbox == Hitbox(new_reference_point, new_width_height_vector.x_hat, new_width_height_vector.y_hat)
         assert egg.is_dead == False
 
     def test_egg_deal_damage(self):
 
-        egghitbox = Hitbox(CartesianPoint(0, 0), 3, 4)
-        egg_config_attacker = EggConfig(hitbox=egghitbox,
-                                        movement_speed=2.0,
-                                        max_health=25.0,
-                                        base_damage=5.0,
-                                        damage_hitbox_scale=1.0,
-                                        invincibility_frames=30)
-        attacker_egg = Egg(egg_config_attacker)
+        egghitbox = Hitbox(CartesianPoint(0,0), 3, 4)
+        egg_config_attacker = EggConfig(hitbox = egghitbox,
+                                       movement_speed = 2.0,
+                                       max_health = 25.0,
+                                       base_damage = 5.0,
+                                       damage_hitbox_scale = 1.0,
+                                       invincibility_frames= 30)
+        attacker_egg = Egg(egg_config_attacker,3)
 
-        eggentityhitbox = Hitbox(CartesianPoint(4, 0), 3, 4)
-        egg_config_target = EggConfig(hitbox=eggentityhitbox,
-                                      movement_speed=2.0,
-                                      max_health=20.0,
-                                      base_damage=1.0,
-                                      damage_hitbox_scale=1.0,
-                                      invincibility_frames=10)
-        target_egg = Egg(egg_config_target)
+        eggentityhitbox = Hitbox(CartesianPoint(4,0), 3, 4)
+        egg_config_target = EggConfig(hitbox = eggentityhitbox,
+                                     movement_speed = 2.0,
+                                     max_health = 20.0,
+                                     base_damage = 1.0,
+                                     damage_hitbox_scale = 1.0,
+                                     invincibility_frames= 10)
+        target_egg = Egg(egg_config_target,3)
 
         initial_target_health = target_egg.health
         attacker_egg.deal_damage(target_egg)
 
-        # not overlapping
+        #not overlapping
         assert target_egg.health == initial_target_health
 
-        attacker_egg.hitbox._coordinate = CartesianPoint(1, 0)
+        attacker_egg.hitbox._coordinate = CartesianPoint(1,0)
         attacker_egg.deal_damage(target_egg)
 
         assert target_egg.health < initial_target_health
         assert target_egg.health == initial_target_health - attacker_egg.base_damage
         assert target_egg._i_frame_counter == target_egg._invincibility_frames
 
-        # invincibility frames
+        #invincibility frames
         target_egg._i_frame_counter = 5
         target_egg_health_invincibility = target_egg.health
         attacker_egg.deal_damage(target_egg)
@@ -74,9 +71,8 @@ class TestEgg:
 
     def test_egg_take_damage(self):
         hitbox = Hitbox(CartesianPoint(0, 0), 3, 4)
-        egg_config = EggConfig(hitbox=hitbox, movement_speed=2.0, max_health=25.0,
-                               base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=2)
-        egg = Egg(egg_config)
+        egg_config = EggConfig(hitbox=hitbox, movement_speed=2.0, max_health=25.0, base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=2)
+        egg = Egg(egg_config,3)
 
         initial_health = egg.health
         damage_amount = 10.0
@@ -97,13 +93,12 @@ class TestEgg:
 
     def test_egg_get_vector_to_hitbox(self):
         egg_hitbox = Hitbox(CartesianPoint(0, 0), 2, 2)
-        egg_config = EggConfig(hitbox=egg_hitbox, movement_speed=1.0, max_health=10.0,
-                               base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=0)
-        egg = Egg(egg_config)
+        egg_config = EggConfig(hitbox=egg_hitbox, movement_speed=1.0, max_health=10.0, base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=0)
+        egg = Egg(egg_config,3)
 
         other_hitbox_above = Hitbox(CartesianPoint(0, -3), 1, 1)
         vector_above = egg._get_vector_to_hitbox(other_hitbox_above)
-
+    
         assert vector_above == Vector(0.0, -2.0)
 
         other_hitbox_below = Hitbox(CartesianPoint(0, 3), 1, 1)
@@ -121,16 +116,15 @@ class TestEgg:
         other_hitbox_overlap = Hitbox(CartesianPoint(0, 0), 3, 3)
         vector_overlap = egg._get_vector_to_hitbox(other_hitbox_overlap)
         assert vector_overlap == Vector(0.0, 0.0)
-
+    
     def test_egg_tick(self):
         hitbox = Hitbox(CartesianPoint(0, 0), 3, 4)
-        egg_config = EggConfig(hitbox=hitbox, movement_speed=2.0, max_health=25.0,
-                               base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=2)
-        egg = Egg(egg_config)
+        egg_config = EggConfig(hitbox=hitbox, movement_speed=2.0, max_health=25.0, base_damage=1.0, damage_hitbox_scale=1.0, invincibility_frames=2)
+        egg = Egg(egg_config,3)
         egg._i_frame_counter = 2
         egg.tick()
         assert egg._i_frame_counter == 1
         egg.tick()
         assert egg._i_frame_counter == 0
         egg.tick()
-        assert egg._i_frame_counter == 0  # Should not go below zero
+        assert egg._i_frame_counter == 0 # Should not go below zero
